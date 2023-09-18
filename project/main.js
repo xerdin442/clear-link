01ca3d08ad085c2a88944eaac5da95861d70a426
xerdin442 <mudianthonio27@gmail.com>
2023-09-19 00:58:21 +0100

    updated codebase

--- project/main.js
+++ project/main.js
@@ -7,3 +7,15 @@ document.querySelector('.faq-container').addEventListener('click', function (eve
         panel.classList.toggle('hidden');
     }
 });
+
+
+const menuToggle = document.getElementById('menu-toggle');
+const sidebar = document.getElementById('sidebar');
+const menuOpenIcon = document.getElementById('menu-open');
+const menuCloseIcon = document.getElementById('menu-close');
+
+menuToggle.addEventListener('click', () => {
+    sidebar.classList.toggle('-translate-x-full');
+    menuOpenIcon.classList.toggle("hidden");
+    menuCloseIcon.classList.toggle("hidden");
+});
